595fdf53b2502fd402d375df2a7ec14bfd17e28e
genemau <genemau@users.noreply.github.com>
2015-08-05 10:22:34 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,44 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+__author__ = 'eugene'
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+from lxml import etree
+import lxml.html
+import string
+from datetime import datetime
+import csv
+import urllib
+import re
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+def getHousesCount(suburb, postCode):
+
+    html = scraperwiki.scrape("http://www.realestate.com.au/buy/with-3-bedrooms-in-" + re.sub(" ","+",suburb) + "%2c+vic+" + postCode + "%3b/list-1?includeSurrounding=false")
+
+    #root = etree.HTML(html)
+
+    root = lxml.html.fromstring(html)
+
+    el = root.cssselect("div.resultsInfo p")[0]
+
+    #for el in root.cssselect("div.resultsInfo p"):
+
+    #    if string.inde
+
+    a = string.split(el.text," ")
+    cnt = a[len(a)-3]
+
+    # Save found data
+    scraperwiki.sqlite.save(unique_keys=['extracted_on','suburb'], data={
+        "extracted_on": extractedOn,
+        "suburb": suburb,
+	"3_bedroom_count": cnt
+        })
+
+    print suburb + " " + a[len(a)-3] 
+
+
+extractedOn = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+dictReader = csv.DictReader(open('Suburbs.csv', 'rb'))
+
+for line in dictReader:
+    getHousesCount(line['Suburb'], line['PostCode'])
+
